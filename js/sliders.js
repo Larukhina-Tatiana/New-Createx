94ae6c991895d1bbc4fa1f61ecb7569d63df8a6c
Татьяна <ltasya72@gmail.com>
2024-06-14 18:02:08 +0300

    работа над clients about.html

--- js/sliders.js
+++ js/sliders.js
@@ -7,6 +7,7 @@ const portfolioSlider = new Swiper(".section__swiper", {
     prevEl: ".portfolio-section__prev",
   },
 });
+
 const testimonialsSlider = new Swiper(".testimonials__swiper", {
   slidesPerView: 1,
   // spaceBetween: 30,
@@ -44,5 +45,48 @@ if (workInsideGalery) {
     thumbs: {
       swiper: workSlider,
     },
+  });
+}
+
+const historySlider = document.querySelector(".history__slider-images");
+
+if (historySlider) {
+  const workSlider = new Swiper(historySlider, {
+    spaceBetween: 20,
+    slidesPerView: 1,
+    navigation: {
+      nextEl: ".history__next",
+      prevEl: ".history__prev",
+    },
+  });
+
+  workSlider.on("slideChange", function () {
+    console.log(workSlider.realIndex);
+
+    historyBtns.forEach((el) => {
+      el.classList.remove("history-nav__btn--active");
+    });
+
+    document
+      .querySelector(`.history-nav__btn[data-index="${workSlider.realIndex}"]`)
+      .classList.add("history-nav__btn--active");
+  });
+
+  const historyBtns = document.querySelectorAll(".history-nav__btn");
+
+  historyBtns.forEach((el, idx) => {
+    el.setAttribute("data-index", idx);
+
+    el.addEventListener("click", (e) => {
+      const index = e.currentTarget.dataset.index;
+
+      historyBtns.forEach((el) => {
+        el.classList.remove("history-nav__btn--active");
+      });
+
+      e.currentTarget.classList.add("history-nav__btn--active");
+
+      workSlider.slideTo(index);
+    });
   });
 }
