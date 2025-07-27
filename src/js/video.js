b6bfca98f158f8fe54e7591ac580584880b1c2f2
Татьяна <ltasya72@gmail.com>
2025-07-27 12:23:11 +0300

    Обновление видео: замена источника видео и улучшение управления воспроизведением

--- src/js/video.js
+++ src/js/video.js
@@ -3,28 +3,46 @@ const videoBlock = document.querySelector(".video__block");
 if (videoBlock) {
   const video = videoBlock.querySelector("video");
   const playBtn = videoBlock.querySelector(".video__button");
-  if (video && playBtn) {
-    // Получаем сохранённое время
-    const savedTime = localStorage.getItem("videoplayer-current-time");
 
-    playBtn.addEventListener("click", () => {
-      videoBlock.classList.add("video__block--played");
+  if (video && playBtn) {
+    let saveInterval;
+
+    function playVideo() {
+      const savedTime = localStorage.getItem("videoplayer-current-time");
       if (savedTime) {
         video.currentTime = parseFloat(savedTime);
       }
       video.play();
-      video.focus();
       video.controls = true;
+      videoBlock.classList.add("video__block--played");
       playBtn.classList.add("video__button--played");
       playBtn.setAttribute("aria-pressed", "true");
-    });
 
-    video.onpause = function () {
-      localStorage.setItem("videoplayer-current-time", video.currentTime);
-      videoBlock.classList.remove("video__block--played");
+      saveInterval = setInterval(() => {
+        localStorage.setItem("videoplayer-current-time", video.currentTime);
+      }, 5000);
+    }
+
+    function pauseVideo() {
+      video.pause();
       video.controls = false;
+      videoBlock.classList.remove("video__block--played");
       playBtn.classList.remove("video__button--played");
       playBtn.setAttribute("aria-pressed", "false");
-    };
+      clearInterval(saveInterval);
+      localStorage.setItem("videoplayer-current-time", video.currentTime);
+    }
+
+    playBtn.addEventListener("click", playVideo);
+
+    video.addEventListener("click", () => {
+      video.paused ? playVideo() : pauseVideo();
+    });
+
+    video.addEventListener("pause", pauseVideo);
+
+    window.addEventListener("beforeunload", () => {
+      localStorage.setItem("videoplayer-current-time", video.currentTime);
+    });
   }
 }
