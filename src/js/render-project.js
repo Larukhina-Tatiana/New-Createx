470bd3eda8bc9a9e9d17451a912554c0566f61d6
Татьяна <ltasya72@gmail.com>
2025-09-24 18:40:31 +0300

    исправлены пути к изображениям в render-project.js

--- src/js/render-project.js
+++ src/js/render-project.js
@@ -76,13 +76,13 @@ function renderGallery(images) {
         <div class="project__cover-big">
           <picture>
             <source type="image/avif" srcset="
-              ../images/projects/${projectId}/${image.src}@1x.avif 1x,
-              ../images/projects/${projectId}/${image.src}@2x.avif 2x">
+              images/projects/${projectId}/${image.src}@1x.avif 1x,
+             images/projects/${projectId}/${image.src}@2x.avif 2x">
             <source type="image/webp" srcset="
-              ../images/projects/${projectId}/${image.src}@1x.webp 1x,
-              ../images/projects/${projectId}/${image.src}@2x.webp 2x">
+              images/projects/${projectId}/${image.src}@1x.webp 1x,
+              images/projects/${projectId}/${image.src}@2x.webp 2x">
             <img class="project__img"
-              src="../images/projects/${projectId}/${image.src}@1x.jpg"
+              src="images/projects/${projectId}/${image.src}@1x.jpg"
               loading="lazy" decoding="async" alt="${image.alt}">
           </picture>
         </div>
@@ -104,13 +104,13 @@ function renderThumbnails(thumbnails) {
         <div class="project__cover-small">
           <picture>
             <source type="image/avif" srcset="
-              ../images/projects/${projectId}/${image.src}@1x.avif 1x,
-              ../images/projects/${projectId}/${image.src}@2x.avif 2x">
+              images/projects/${projectId}/${image.src}@1x.avif 1x,
+              images/projects/${projectId}/${image.src}@2x.avif 2x">
             <source type="image/webp" srcset="
-              ../images/projects/${projectId}/${image.src}@1x.webp 1x,
-              ../images/projects/${projectId}/${image.src}@2x.webp 2x">
+              images/projects/${projectId}/${image.src}@1x.webp 1x,
+              images/projects/${projectId}/${image.src}@2x.webp 2x">
             <img class="project__img project__img--small"
-              src="../images/projects/${projectId}/${image.src}@1x.jpg"
+              src="images/projects/${projectId}/${image.src}@1x.jpg"
               loading="lazy" decoding="async" alt="${image.alt}">
           </picture>
         </div>
