caff66f9e7f617ee6c943726c83b6f827c460783
Татьяна <ltasya72@gmail.com>
2025-11-04 17:16:03 +0200

    Пределана анимация на секцию facts

--- docs/js/progress.js
+++ docs/js/progress.js
@@ -30,59 +30,119 @@
 //   }
 // });
 
+// document.addEventListener("DOMContentLoaded", function () {
+//   const circles = document.querySelectorAll(".facts-element__circle");
+
+//   const observer = new IntersectionObserver(
+//     (entries) => {
+//       entries.forEach((entry) => {
+//         if (entry.isIntersecting) {
+//           const circleElement = entry.target;
+//           const progress = circleElement.querySelector(".progress");
+//           const valueBlock = circleElement.querySelector(
+//             ".facts-element__value"
+//           );
+//           const radius = progress.getAttribute("r");
+//           const circleLength = 2 * Math.PI * radius;
+
+//           if (circleElement.dataset.percentage === "true") {
+//             const full = parseFloat(circleElement.dataset.full);
+//             const value = parseFloat(circleElement.dataset.value);
+//             const percentageProgress = Math.floor((value / full) * 100);
+
+//             valueBlock.textContent = value;
+//             progress.setAttribute("stroke-dasharray", circleLength);
+
+//             setTimeout(() => {
+//               progress.style.strokeDashoffset =
+//                 circleLength - (circleLength * percentageProgress) / 100;
+//             }, 10);
+//           } else {
+//             const percent = parseFloat(circleElement.dataset.percent);
+//             const percentageProgress = Math.floor(percent);
+
+//             valueBlock.textContent = percent + "%";
+//             progress.setAttribute("stroke-dasharray", circleLength);
+
+//             setTimeout(() => {
+//               progress.style.strokeDashoffset =
+//                 circleLength - (circleLength * percentageProgress) / 100;
+//             }, 10);
+//           }
+
+//           // Добавляем класс для запуска анимации значения
+//           setTimeout(() => {
+//             valueBlock.classList.add("show");
+//           }, 1000);
+
+//           observer.unobserve(entry.target);
+//         }
+//       });
+//     },
+//     { threshold: 0.5 }
+//   );
+
+//   circles.forEach((circle) => {
+//     observer.observe(circle);
+//   });
+// });
+
 document.addEventListener("DOMContentLoaded", function () {
-  const circles = document.querySelectorAll(".facts-element__circle");
-
-  const observer = new IntersectionObserver(
-    (entries) => {
-      entries.forEach((entry) => {
-        if (entry.isIntersecting) {
-          const circleElement = entry.target;
-          const progress = circleElement.querySelector(".progress");
-          const valueBlock = circleElement.querySelector(
-            ".facts-element__value"
-          );
-          const radius = progress.getAttribute("r");
-          const circleLength = 2 * Math.PI * radius;
-
-          if (circleElement.dataset.percentage === "true") {
-            const full = parseFloat(circleElement.dataset.full);
-            const value = parseFloat(circleElement.dataset.value);
-            const percentageProgress = Math.floor((value / full) * 100);
-
-            valueBlock.textContent = value;
-            progress.setAttribute("stroke-dasharray", circleLength);
-
-            setTimeout(() => {
-              progress.style.strokeDashoffset =
-                circleLength - (circleLength * percentageProgress) / 100;
-            }, 10);
-          } else {
-            const percent = parseFloat(circleElement.dataset.percent);
-            const percentageProgress = Math.floor(percent);
-
-            valueBlock.textContent = percent + "%";
-            progress.setAttribute("stroke-dasharray", circleLength);
-
-            setTimeout(() => {
-              progress.style.strokeDashoffset =
-                circleLength - (circleLength * percentageProgress) / 100;
-            }, 10);
+  let isFactsAnimatorFinished = false;
+  if (document.querySelector(".facts")) {
+    const factsSection = document.querySelector(".facts");
+    const observer = new IntersectionObserver(
+      (entries) => {
+        entries.forEach((entry) => {
+          if (entry.isIntersecting) {
+            // console.log("entry", entry);
+            factsSection.classList.add("svg-animation");
+            isFactsAnimatorFinished = false;
+            animateValue(".facts__item:nth-child(1) .facts-element__value");
+            animateValue(
+              ".facts__item:nth-child(2) .facts-element__value",
+              2200
+            );
+            animateValue(
+              ".facts__item:nth-child(3) .facts-element__value",
+              2400
+            );
+            animateValue(
+              ".facts__item:nth-child(4) .facts-element__value",
+              2600
+            );
+            // isFactsAnimatorFinished = true;
+          } else if (!isFactsAnimatorFinished) {
+            factsSection.classList.remove("svg-animation");
           }
+        });
+      },
+      { threshold: 0.75 }
+    );
+    // говорим следить за сецией
+    observer.observe(factsSection);
+  }
 
-          // Добавляем класс для запуска анимации значения
-          setTimeout(() => {
-            valueBlock.classList.add("show");
-          }, 1000);
+  function animateValue(selector, duration = 2000) {
+    const valueEl = document.querySelector(selector);
+    // console.log("valueEl", valueEl);
 
-          observer.unobserve(entry.target);
-        }
-      });
-    },
-    { threshold: 0.5 }
-  );
+    if (!valueEl || isFactsAnimatorFinished) return;
 
-  circles.forEach((circle) => {
-    observer.observe(circle);
-  });
+    const percent = valueEl.getAttribute("data-percent");
+    const isPercent = percent.includes("%");
+    const to = parseFloat(percent);
+    const startTime = performance.now();
+    function update(now) {
+      const progress = Math.min((now - startTime) / duration, 1);
+      let current = Math.floor(progress * to);
+      valueEl.textContent = isPercent ? current + "%" : current;
+      if (progress < 1) {
+        requestAnimationFrame(update);
+      } else {
+        valueEl.textContent = isPercent ? to + "%" : to;
+      }
+    }
+    requestAnimationFrame(update);
+  }
 });
